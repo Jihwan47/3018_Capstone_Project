de186238d1ce0641de589611b34c80a8803c758d
JayHwang <hjik7297@gmail.com>
2026-04-05 14:10:27 -0500

    feat: added restaurant order model

---
+++ src/api/v1/models/orderModel.ts
@@ -0,0 +1,47 @@
+
+/**
+ * Represents enumeration of order status
+ * Pending, In Progress, Completed, Cancelled
+ * Default value of pending once order has been placed.
+ */
+export enum OrderStatus {
+    Pending = "Pending",
+    InProgress = "In Progress",
+    Completed = "Completed",
+    Cancelled = "Cancelled"
+}
+
+/**
+ * Represents request format of order interface (Only for pickup)
+ * @param restaurantId - ID of the restaurant receiving the order
+ * @param restaurantName - name of the restaurant receiving the order
+ * @param customerName - name of the customer placing the order
+ * @param items - list of items in the order with their quantity
+ * @param totalPrice - total price of the order
+ */
+export interface createOrder {
+    restaurantId: string;
+    restaurantName: string;
+    customerName: string;
+    items: { itemId: string; quantity: number }[];
+    totalPrice: number;
+}
+
+/**
+ * Represents request format of order interface (Only for pickup)
+ * @param restaurantId - ID of the restaurant receiving the order
+ * @param restaurantName - name of the restaurant receiving the order
+ * @param customerId - ID of the customer placing the order
+ * @param customerName - name of the customer placing the order
+ * @param items - list of items in the order with their quantity
+ * @param totalPrice - total price of the order
+ * @param status - status of the order
+ */
+export interface Order {
+    restaurantId: string;
+    restaurantName: string;
+    customerName: string;
+    items: { itemId: string; itemName: string; quantity: number }[];
+    totalPrice: number;
+    status: OrderStatus;
+}
